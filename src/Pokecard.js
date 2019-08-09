5fd8a3d2e5164f69bf4e8e51b389774de02098ab
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-08 20:55:49 -0700

    2. Create Pokecard and add some basic style to it

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react'
+import './Pokecard.css'
 class Pokecard  extends Component{
+    pokemons = [
+        {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+        {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+        {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+        {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+        {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+        {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+        {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+        {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+    ]
     render() {
         return(
-            <div>I'm pokecard</div>
+            <div className="Pokecard">
+                <h3 className="Pokecard-name">Pikachu</h3>
+                <img  className='Pokecard-img' src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${'00'}1.png`} alt="pictue of pokemon"/>
+                <p>Type: electric</p>
+                <p>EXP: 112</p>
+            </div>
         )
     }
 }
